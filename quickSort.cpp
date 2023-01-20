e17f3c36f2c79f3ceaaa30e49044988bfb504c18
Aryan Thapliyal <94701379+Saint-Potato@users.noreply.github.com>
2023-01-20 12:50:51 +0530

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+using namespace std;
 
-int L_partition(int *arr, int f, int l){
+
+int L_partition(int *arr, int f, int l){  // Lomuto partition
     int pivot = arr[l]; // sets last element as pivot
     int i = f - 1;      // i keeps count of first sub array, i.e. elements smaller than pivot
 
     for(int j = f; j < l; j++){  // j traverses through array and tells which elements have been compared against pivot
-        if(arr[j] < = pivot){
+        if(arr[j] <= pivot){
             i++;
             swap(arr[i], arr[j]);    // if J-th element is smaller than pivot, it is swapped to last of first sub array, i.e. elements smaller than pivot and the sub array is extended
         }
@@ -16,13 +18,49 @@ int L_partition(int *arr, int f, int l){
     return i+1;                 // index of pivot is returned
 }
 
+int H_partition(int *arr, int f, int l){
+    int x = arr[f];
+    int i = f - 1;
+    int j = l + 1;
+
+    while(true){
+        do{
+            j--;
+        }
+        while(arr[j] > x);
+
+        do{
+            i++;
+        }
+        while(arr[i] < x);
+
+        if(i < j){
+            swap(arr[i], arr[j]);
+        }
+        else{
+            swap(arr[j], arr[f]);
+            return j;
+        }
+    }
+}
+
 void quickSort(int *arr, int f, int l){
     if(f >= l){                     // base condition 
         return;
     }
 
-    int p = L_partition(arr, f, l);                      // partition into 2 sub arrays such that element of first sub array are smaller than all element of 2nd sub array
+    int p = H_partition(arr, f, l);                      // partition into 2 sub arrays such that element of first sub array are smaller than all element of 2nd sub array
 
     quickSort(arr, f, p-1);                              // perform quickSort on first sub array
-    quickSort(arr, p+1, l);                              // perform quickSort on 2nd sub array
+    quickSort(arr, p + 1, l);                              // perform quickSort on 2nd sub array
+}
+
+int main(){
+    int arr[6] = {8, 4, 2, 9, 12, 1};
+    quickSort(arr, 0, 5);
+    for(int i = 0; i < 6; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
 }
